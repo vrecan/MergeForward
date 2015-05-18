42eaf72cfeb34c8fa2c7b168f202a1f8e97564b2
Ben Aldrich <ben.aldrich@logrhythm.com>
2015-05-18 15:57:21 -0600

    main loop

---
+++ mergeforward.go
@@ -0,0 +1,63 @@
+package main
+
+// Merges files together and prints result to stdout
+
+import (
+	"flag"
+	"fmt"
+	"github.com/vrecan/MergeForward/merge"
+	"io/ioutil"
+	"os"
+)
+
+var src = flag.String("src", "", "Source configuration file. Src values are prefered over dest.")
+var dst = flag.String("dst", "", "Destination configuration file.")
+var split = flag.String("split", ":", "What character we should use to split key value pairs")
+
+type conf interface{}
+
+func main() {
+	flag.Parse()
+	if len(*src) == 0 {
+		fmt.Println("No source file.")
+		os.Exit(1)
+	}
+	if len(*dst) == 0 {
+		fmt.Println("No destination file.")
+		os.Exit(1)
+	}
+
+	srcBytes, err := ioutil.ReadFile(*src)
+	if nil != err {
+		fmt.Println("Unable to read src file: ", err)
+		os.Exit(1)
+	}
+
+	dstBytes, err := ioutil.ReadFile(*dst)
+	if nil != err {
+		fmt.Println("Unable to read destination file: ", err)
+		os.Exit(1)
+	}
+
+	result, err := merge.SimpleMerge(string(srcBytes), string(dstBytes), *split)
+	if nil != err {
+		fmt.Println("Unable to merge files: ", err)
+		os.Exit(1)
+	} else {
+		fmt.Print(result)
+	}
+	// fmt.Println(result)
+	// fmt.Println(string(srcBytes))
+	// result, err := merge.DiffMerge(string(srcBytes), string(dstBytes))
+	// if nil != err {
+
+	// }
+	// finalResult, err := merge.DiffMerge(result, string(dstBytes))
+	// if nil != err {
+
+	// }
+
+	// fmt.Println(finalResult)
+
+	// merge.MarshalToYaml(result, "/tmp/output.yml")
+}
