2bcc3386961bf5c6ced1adc3f332b6e344c1866a
Tanner Moore <tanner.moore@logrhythm.com>
2015-07-28 22:21:29 -0600

    log timestamps

--- mergeforward.go
+++ mergeforward.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 	"os"
 	"log"
-	"io"
 
 )
 
@@ -18,44 +17,46 @@ var src = flag.String("src", "", "Source configuration file. Src values are pref
 var dst = flag.String("dst", "", "Destination configuration file.")
 var split = flag.String("split", ":", "Splitter for key value pairs")
 var config = flag.String("config", "./c.ini", "MergeForward configuration ini file")
-var outputdir = flag.String("outputdir", "", "The output directory of the log file")
+var outputdir = flag.String("outputdir", ".", "The output directory of the log file")
 
 type conf interface{}
 
 func main() {
 	flag.Parse()
 
+	fmt.Println("Log file will be created at", *outputdir + string(os.PathSeparator) + "MergeForward.log")
+
 	_ = os.Remove(*outputdir + "MergeForward.log")
-	logFile, err := os.OpenFile(*outputdir + "MergeForward.log", os.O_RDWR | os.O_CREATE, 0666)
+	logFile, err := os.OpenFile(*outputdir + string(os.PathSeparator) + "MergeForward.log", os.O_RDWR | os.O_CREATE, 0666)
 	if err != nil {
-		fmt.Println("Error making log file: " + *outputdir + string(os.PathSeparator) + "MergeForward.log")
+		fmt.Println("Error making log file:", *outputdir + string(os.PathSeparator) + "MergeForward.log")
 	}
 	defer logFile.Close()
-	
-	dualLogger := log.New(io.MultiWriter(logFile,os.Stdout), "", 0)
+
+	log.SetOutput(logFile)
 
 	if len(*src) == 0 {
-		dualLogger.Fatalln("No source file")
+		log.Fatalln("No source file")
 	}
 	if len(*dst) == 0 {
-		dualLogger.Fatalln("No destination file.")
+		log.Fatalln("No destination file.")
 	}
 	conf := c.GetConf(*config)
 
 	srcBytes, err := ioutil.ReadFile(*src)
 	if nil != err {
-		dualLogger.Fatalln("Unable to read src file: ", err)
+		log.Fatalln("Unable to read src file: ", err)
 	}
 
 	dstBytes, err := ioutil.ReadFile(*dst)
 	if nil != err {
-		dualLogger.Fatalln("Unable to read destination file: ", err)
+		log.Fatalln("Unable to read destination file: ", err)
 	}
 
 	result, err := merge.SimpleMerge(string(srcBytes), string(dstBytes), *split, conf, logFile)
 	if nil != err {
-		dualLogger.Fatalln("Unable to merge files: ", err)
+		log.Fatalln("Unable to merge files: ", err)
 	} else {
-		dualLogger.Println(result)
+		log.Println(result)
 	}
 }
