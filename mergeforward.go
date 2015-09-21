1eb9815d77c18a4de0610760d982b5bc61b3b20a
Tanner Moore <tanner.moore@logrhythm.com>
2015-09-21 16:03:21 -0600

    Formatting

--- mergeforward.go
+++ mergeforward.go
@@ -10,8 +10,8 @@ import (
 	"io/ioutil"
 	"os"
 	"github.com/cihub/seelog"
-    "path/filepath"
-    "log"
+	"path/filepath"
+	"log"
 )
 
 var src = flag.String("src", "", "Source configuration file. Src values are prefered over dest.")
@@ -24,77 +24,76 @@ type conf interface{}
 
 func main() {
 
-    currentDirectory, err := filepath.Abs(filepath.Dir(os.Args[0]))
-    if err != nil {
-        log.Fatal(err, "- This error happened while automatically detecting the current directory of mergeforward")
-    }
+	currentDirectory, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err, "- This error happened while automatically detecting the current directory of mergeforward")
+	}
 
-    fmt.Print("Printing current directory: ")
-    fmt.Println(currentDirectory)
+	fmt.Print("Printing current directory: ")
+	fmt.Println(currentDirectory)
 
 	logger, err := seelog.LoggerFromConfigAsString(
-        "<seelog type=\"asynctimer\" asyncinterval=\"1000000\">" +
-	        "<outputs formatid=\"all\">" +
-	            "<filter levels=\"info\" formatid=\"fmtinfo\">" +
-	                "<console/>" +
-	                "<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
-	            "</filter>" +
-	            "<filter levels=\"warn\" formatid=\"fmtwarn\">" +
-	                "<console/>" +
-	                "<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
-	            "</filter>" +
-	            "<filter levels=\"error,critical\" formatid=\"fmterror\">" +
-	                "<console/>" +
-	                "<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
-	            "</filter>" +
-	        "</outputs>" +
-            "<formats>" +
-	            "<format id=\"fmtinfo\" format=\"%EscM(32)[%Level]%EscM(0) [%Date %Time] [%File] %Msg%n\"/>" +
-	            "<format id=\"fmterror\" format=\"%EscM(31)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
-	            "<format id=\"fmtwarn\" format=\"%EscM(33)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
-	            "<format id=\"all\" format=\"%EscM(2)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
-	        "</formats>" +
-        "</seelog>")
+		"<seelog type=\"asynctimer\" asyncinterval=\"1000000\">" +
+			"<outputs formatid=\"all\">" +
+				"<filter levels=\"info\" formatid=\"fmtinfo\">" +
+					"<console/>" +
+					"<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
+				"</filter>" +
+				"<filter levels=\"warn\" formatid=\"fmtwarn\">" +
+					"<console/>" +
+					"<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
+				"</filter>" +
+				"<filter levels=\"error,critical\" formatid=\"fmterror\">" +
+					"<console/>" +
+					"<rollingfile type=\"size\" filename=\"MergeForward.log\" maxsize=\"20000000\" maxrolls=\"5\" />" +
+				"</filter>" +
+			"</outputs>" +
+			"<formats>" +
+				"<format id=\"fmtinfo\" format=\"%EscM(32)[%Level]%EscM(0) [%Date %Time] [%File] %Msg%n\"/>" +
+				"<format id=\"fmterror\" format=\"%EscM(31)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
+				"<format id=\"fmtwarn\" format=\"%EscM(33)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
+				"<format id=\"all\" format=\"%EscM(2)[%LEVEL]%EscM(0) [%Date %Time] [%FuncShort @ %File.%Line] %Msg%n\"/>" +
+			"</formats>" +
+		"</seelog>")
 
 	flag.Parse()
 
 	os.MkdirAll(*outputdir, 0666)
 
 	_ = os.Remove(*outputdir + "MergeForward.log")
-//	logFile, err := os.OpenFile(*outputdir + string(os.PathSeparator) + "MergeForward.log", os.O_RDWR | os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("Error making log file:", *outputdir + string(os.PathSeparator) + "MergeForward.log")
 	}
 	defer logger.Close()
 
 	if len(*src) == 0 {
-        logger.Critical("No source file")
-        os.Exit(-1)
+		logger.Critical("No source file")
+		os.Exit(1)
 	}
 	if len(*dst) == 0 {
-        logger.Critical("No destination file.")
-        os.Exit(-1)
+		logger.Critical("No destination file.")
+		os.Exit(1)
 	}
 	conf := c.GetConf(*config)
 
 	srcBytes, err := ioutil.ReadFile(*src)
 	if nil != err {
-        logger.Critical("Unable to read src file: ", err)
-        os.Exit(-1)
+		logger.Critical("Unable to read src file: ", err)
+		os.Exit(1)
 	}
 
 	dstBytes, err := ioutil.ReadFile(*dst)
 	if nil != err {
 		logger.Critical("Unable to read destination file: ", err)
-        os.Exit(-1)
+		os.Exit(1)
 	}
 
 	result, err := merge.SimpleMerge(string(srcBytes), string(dstBytes), *split, conf, logger)
 	if nil != err {
 		logger.Critical("Unable to merge files: ", err)
-        os.Exit(-1)
+		os.Exit(1)
 	} else {
 		fmt.Println(result)
-        logger.Info("Final file output:\n" + result)
+		logger.Info("Final file output:\n" + result)
 	}
 }
